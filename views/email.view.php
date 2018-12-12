94b012bc354fd5140a66b539da09ced10d48f6c0
mikey <manicdesigner@gmail.com>
2018-12-12 15:54:49 -0500

    using a stub for the header now.

--- views/email.view.php
+++ views/email.view.php
@@ -10,24 +10,7 @@
  */
 
 ?>
-
-    <!doctype html>
-    <html lang="en">
-    <head>
-
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="keywords" content="garys tools, tools contact, bradenton tool sale">
-        <meta name="description" content="Garys tools, retired bodyman selling his body tools locally in bradenton fl.">
-        <link href="https://fonts.googleapis.com/css?family=Supermercado+One" rel="stylesheet">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link rel="stylesheet" href="<?php echo url_for(node_module('public/css/main.css')); ?>">
-
-        <title><?php echo $page_title; ?></title>
-    </head>
-<body>
+<?php include(SHARED_PATH . '/header.php'); ?>
 <header role="banner" class='container-fluid'>
     <section class='row'>
         <h1 class="col-12 col-md-4 tools logo__section logo">
