c8e1002cfd288a38922ce889f757e1082ed1eb0d
mikey <manicdesigner@gmail.com>
2018-12-18 11:46:12 -0500

    rm hotjar

--- private/shared/header.php
+++ private/shared/header.php
@@ -13,7 +13,6 @@ if (!isset($page_title)) { $page_title = 'Tools Site'; }
 <!doctype html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
