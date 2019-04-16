0bf820d6fb32639305fd0505ce4a639c92f915f3
mikey <manicdesigner@gmail.com>
2019-04-16 15:57:28 -0400

    trying some js

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,26 @@
-// $("a[href='#top']").click(function() {
-//     $('body, html').animate({ scrollTop: 0 }, 2000);
-//     return false;
-// });
 
-$(document).ready(function) {
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+// --!!! fix for dropdowns to work!!!!----//
+let parts = document.getElementById("tool-parts");
+parts.addEventListener("click", dropdown);
 
 
-    $('a[href=#top]').click(function () {
-        $("html, body").animate({scrollTop: 0}, 'slow');
-        return false;
-    });
+function dropdown() {
+    document.getElementById("toolPartsDropDown").classList.toggle("show");
+}
 
-});
+// Close the dropdown menu if the user clicks outside of it
+Window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        console.log('drop');
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
